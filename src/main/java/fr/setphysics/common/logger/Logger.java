50dc1d545cd629d9efd165f5cac20fb3799da12e
Feavy <quentin071100@live.fr>
2021-05-17 20:16:50 +0200

    Logger: debug/info/warning/error with colors. Easy API.

--- src/main/java/fr/setphysics/common/logger/Logger.java
+++ src/main/java/fr/setphysics/common/logger/Logger.java
@@ -1,11 +1,19 @@
 package fr.setphysics.common.logger;
 
 public class Logger {
-    public static void main(String[] args) {
-        Logger.info("test");
+    public static void debug(String message) {
+        System.out.println("[DEBUG] "+Thread.currentThread().getStackTrace()[2] + " : " + message);
     }
 
     public static void info(String message) {
-        System.out.println("[INFO] "+Thread.currentThread().getStackTrace()[2] + " : " + message);
+        System.out.println("\u001B[34m[INFO] "+Thread.currentThread().getStackTrace()[2] + " : " + message+"\u001B[0m");
+    }
+
+    public static void warning(String message) {
+        System.out.println("\u001B[33m[WARNING] "+Thread.currentThread().getStackTrace()[2] + " : " + message+"\u001B[0m");
+    }
+
+    public static void error(String message) {
+        System.out.println("\u001B[31m[ERROR] "+Thread.currentThread().getStackTrace()[2] + " : " + message+"\u001B[0m");
     }
 }
