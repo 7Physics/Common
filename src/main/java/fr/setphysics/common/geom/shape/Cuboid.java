e0be617a00b73c29f5a1a3775ed6202caa3955ab
Feavy <quentin071100@live.fr>
2021-05-22 21:35:32 +0200

    Added missing cuboid faces.

--- src/main/java/fr/setphysics/common/geom/shape/Cuboid.java
+++ src/main/java/fr/setphysics/common/geom/shape/Cuboid.java
@@ -15,14 +15,56 @@ public class Cuboid extends Shape {
         /**
          * Appelle le super dans param et crée les 8 vertices
          */
-        addVertex(new Vec3(-width/2, -height/2, -length/2));
-        addVertex(new Vec3(-width/2, -height/2, length/2));
-        addVertex(new Vec3(width/2, -height/2, length/2));
-        addVertex(new Vec3(width/2, -height/2, -length/2));
 
-        addVertex(new Vec3(width/2, height/2, -length/2));
-        addVertex(new Vec3(width/2, height/2, length/2));
-        addVertex(new Vec3(-width/2, height/2, length/2));
-        addVertex(new Vec3(-width/2, height/2, -length/2));
+        /*
+        Parallélépipède rectangle A B C D E F G H
+        https://ibb.co/mTgNb5q
+         */
+
+        Vec3 a = new Vec3(-width/2, -height/2, -length/2);
+        Vec3 b = new Vec3(-width/2, -height/2, length/2);
+        Vec3 c = new Vec3(width/2, -height/2, length/2);
+        Vec3 d = new Vec3(width/2, -height/2, -length/2);
+
+        Vec3 e = new Vec3(-width/2, height/2, -length/2);
+        Vec3 f = new Vec3(-width/2, height/2, length/2);
+        Vec3 g = new Vec3(width/2, height/2, length/2);
+        Vec3 h = new Vec3(width/2, height/2, -length/2);
+
+        // Face inférieure
+        addVertex(a);
+        addVertex(b);
+        addVertex(c);
+        addVertex(d);
+
+        // Face devant
+        addVertex(d);
+        addVertex(a);
+        addVertex(e);
+        addVertex(h);
+
+        // Face gauche
+        addVertex(h);
+        addVertex(d);
+        addVertex(c);
+        addVertex(g);
+
+        // Face supérieure
+        addVertex(g);
+        addVertex(h);
+        addVertex(e);
+        addVertex(f);
+
+        // Face derriere
+        addVertex(f);
+        addVertex(g);
+        addVertex(c);
+        addVertex(b);
+
+        // Face droite
+        addVertex(b);
+        addVertex(f);
+        addVertex(e);
+        addVertex(a);
     }
 }
