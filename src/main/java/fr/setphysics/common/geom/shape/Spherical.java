016edf0b437512f6df7d1b9a65b9b3633f3add9b
TURC Eléa <eleaturc@outlook.fr>
2021-05-25 08:54:42 +0200

    First try spherical

--- src/main/java/fr/setphysics/common/geom/shape/Spherical.java
+++ src/main/java/fr/setphysics/common/geom/shape/Spherical.java
@@ -16,10 +16,46 @@ public class Spherical extends Shape {
         Vec3 f = new Vec3(-first_coords/2, first_coords/2, first_coords/2);
         Vec3 g = new Vec3(first_coords/2, first_coords/2, first_coords/2);
         Vec3 h = new Vec3(first_coords/2, first_coords/2, -first_coords/2);
+        
+     // Face inférieure
+        addVertex(a);
+        addVertex(b);
+        addVertex(c);
+        addVertex(d);
+
+        // Face devant
+        addVertex(d);
+        addVertex(a);
+        addVertex(e);
+        addVertex(h);
+
+        // Face gauche
+        addVertex(h);
+        addVertex(d);
+        addVertex(c);
+        addVertex(g);
+
+        // Face supérieure
+        addVertex(g);
+        addVertex(h);
+        addVertex(e);
+        addVertex(f);
+
+        // Face derriere
+        addVertex(f);
+        addVertex(g);
+        addVertex(c);
+        addVertex(b);
+
+        // Face droite
+        addVertex(b);
+        addVertex(f);
+        addVertex(e);
+        addVertex(a);
 
     }
 
-    private Vec3 divide_faces(Vec3 a, Vec3 b, Vec3 c, Vec3 d) {
-
-    }
+//    private Vec3 divide_faces(Vec3 a, Vec3 b, Vec3 c, Vec3 d) {
+//
+//    }
 }
