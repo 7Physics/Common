01599f92938ffd842c3ea7215028bf2e3b41936e
TURC Eléa <eleaturc@outlook.fr>
2021-05-25 09:37:01 +0200

    Second try spherical

--- src/main/java/fr/setphysics/common/geom/shape/Spherical.java
+++ src/main/java/fr/setphysics/common/geom/shape/Spherical.java
@@ -1,61 +1,72 @@
 package fr.setphysics.common.geom.shape;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.setphysics.common.geom.Shape;
 import fr.setphysics.common.geom.Vec3;
 
 public class Spherical extends Shape {
-    public Spherical(double radius) {
-        super();
-        double first_coords = radius / Math.sqrt(2);
-        Vec3 a = new Vec3(-first_coords/2, -first_coords/2, -first_coords/2);
-        Vec3 b = new Vec3(-first_coords/2, -first_coords/2, first_coords/2);
-        Vec3 c = new Vec3(first_coords/2, -first_coords/2, first_coords/2);
-        Vec3 d = new Vec3(first_coords/2, -first_coords/2, -first_coords/2);
+	public Spherical(double radius) {
+		super();
+		List<Vec3> test = new ArrayList<Vec3>();
+		double first_coords = radius / Math.sqrt(2);
+		Vec3 a = new Vec3(-first_coords / 2, -first_coords / 2, -first_coords / 2);
+		Vec3 b = new Vec3(-first_coords / 2, -first_coords / 2, first_coords / 2);
+		Vec3 c = new Vec3(first_coords / 2, -first_coords / 2, first_coords / 2);
+		Vec3 d = new Vec3(first_coords / 2, -first_coords / 2, -first_coords / 2);
 
-        Vec3 e = new Vec3(-first_coords/2, first_coords/2, -first_coords/2);
-        Vec3 f = new Vec3(-first_coords/2, first_coords/2, first_coords/2);
-        Vec3 g = new Vec3(first_coords/2, first_coords/2, first_coords/2);
-        Vec3 h = new Vec3(first_coords/2, first_coords/2, -first_coords/2);
-        
-     // Face inférieure
-        addVertex(a);
-        addVertex(b);
-        addVertex(c);
-        addVertex(d);
+		Vec3 e = new Vec3(-first_coords / 2, first_coords / 2, -first_coords / 2);
+		Vec3 f = new Vec3(-first_coords / 2, first_coords / 2, first_coords / 2);
+		Vec3 g = new Vec3(first_coords / 2, first_coords / 2, first_coords / 2);
+		Vec3 h = new Vec3(first_coords / 2, first_coords / 2, -first_coords / 2);
 
-        // Face devant
-        addVertex(d);
-        addVertex(a);
-        addVertex(e);
-        addVertex(h);
+		// Face inférieure
+		addVertex(a);
+		addVertex(b);
+		addVertex(c);
+		addVertex(d);
+		test = findPoints(a,b,c,d);
+		test.forEach(p -> addVertex(p));
 
-        // Face gauche
-        addVertex(h);
-        addVertex(d);
-        addVertex(c);
-        addVertex(g);
+		// Face devant
+		addVertex(d);
+		addVertex(a);
+		addVertex(e);
+		addVertex(h);
 
-        // Face supérieure
-        addVertex(g);
-        addVertex(h);
-        addVertex(e);
-        addVertex(f);
+		// Face gauche
+		addVertex(h);
+		addVertex(d);
+		addVertex(c);
+		addVertex(g);
 
-        // Face derriere
-        addVertex(f);
-        addVertex(g);
-        addVertex(c);
-        addVertex(b);
+		// Face supérieure
+		addVertex(g);
+		addVertex(h);
+		addVertex(e);
+		addVertex(f);
 
-        // Face droite
-        addVertex(b);
-        addVertex(f);
-        addVertex(e);
-        addVertex(a);
+		// Face derriere
+		addVertex(f);
+		addVertex(g);
+		addVertex(c);
+		addVertex(b);
 
-    }
+		// Face droite
+		addVertex(b);
+		addVertex(f);
+		addVertex(e);
+		addVertex(a);
 
-//    private Vec3 divide_faces(Vec3 a, Vec3 b, Vec3 c, Vec3 d) {
-//
-//    }
+	}
+
+	private List<Vec3> findPoints(Vec3 a, Vec3 b, Vec3 c, Vec3 d) {
+		List<Vec3> res = new ArrayList<Vec3>();
+		res.add(new Vec3((a.getX() + b.getX()) / 2, (a.getY() + b.getY()) / 2, (a.getZ() + b.getZ()) / 2));
+		res.add(new Vec3((b.getX() + c.getX()) / 2, (b.getY() + c.getY()) / 2, (b.getZ() + c.getZ()) / 2));
+		res.add(new Vec3((c.getX() + d.getX()) / 2, (c.getY() + d.getY()) / 2, (c.getZ() + d.getZ()) / 2));
+		res.add(new Vec3((a.getX() + d.getX()) / 2, (a.getY() + d.getY()) / 2, (a.getZ() + d.getZ()) / 2));
+		return res;
+	}
 }
