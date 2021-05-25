42a4bc880050827c7e1a68972e5a4f34061921b4
TURC Eléa <eleaturc@outlook.fr>
2021-05-25 14:37:36 +0200

    Try n°5

--- src/main/java/fr/setphysics/common/geom/shape/Spherical.java
+++ src/main/java/fr/setphysics/common/geom/shape/Spherical.java
@@ -7,8 +7,11 @@ import fr.setphysics.common.geom.Shape;
 import fr.setphysics.common.geom.Vec3;
 
 public class Spherical extends Shape {
+	private double radius;
+
 	public Spherical(double radius) {
 		super();
+		this.radius = radius;
 		List<Vec3> pointsList = new ArrayList<Vec3>();
 		double first_coords = radius / Math.sqrt(2);
 		Vec3 a = new Vec3(-first_coords / 2, -first_coords / 2, -first_coords / 2);
@@ -37,35 +40,48 @@ public class Spherical extends Shape {
 
 		pointsList.forEach(p -> addVertex(p));
 	}
+	
+	private Vec3 pointOnSphere(Vec3 pointOnCube) {
+		double ratio = this.radius
+				/ Math.sqrt(Math.pow(pointOnCube.getX(), 2) + Math.pow(pointOnCube.getY(), 2) + Math.pow(pointOnCube.getZ(), 2));
+
+		return new Vec3(pointOnCube.getX() * ratio, pointOnCube.getY() * ratio, pointOnCube.getZ() * ratio);
+	}
 
 	private List<Vec3> findPoints(Vec3 a, Vec3 b, Vec3 c, Vec3 d) {
 		List<Vec3> res = new ArrayList<Vec3>();
-		Vec3 p1 = new Vec3((a.getX() + b.getX()) / 2, (a.getY() + b.getY()) / 2, (a.getZ() + b.getZ()) / 2);
-		Vec3 p2 = new Vec3((b.getX() + c.getX()) / 2, (b.getY() + c.getY()) / 2, (b.getZ() + c.getZ()) / 2);
-		Vec3 p3 = new Vec3((c.getX() + d.getX()) / 2, (c.getY() + d.getY()) / 2, (c.getZ() + d.getZ()) / 2);
-		Vec3 p4 = new Vec3((a.getX() + d.getX()) / 2, (a.getY() + d.getY()) / 2, (a.getZ() + d.getZ()) / 2);
-		Vec3 p5 = new Vec3((p1.getX() + p3.getX()) / 2, (p1.getY() + p3.getY()) / 2, (p1.getZ() + p3.getZ()) / 2);
-		
-		res.add(p5);
-		res.add(p4);
+		Vec3 mAB = new Vec3((a.getX() + b.getX()) / 2, (a.getY() + b.getY()) / 2, (a.getZ() + b.getZ()) / 2);
+		Vec3 mBC = new Vec3((b.getX() + c.getX()) / 2, (b.getY() + c.getY()) / 2, (b.getZ() + c.getZ()) / 2);
+		Vec3 mCD = new Vec3((c.getX() + d.getX()) / 2, (c.getY() + d.getY()) / 2, (c.getZ() + d.getZ()) / 2);
+		Vec3 mAD = new Vec3((a.getX() + d.getX()) / 2, (a.getY() + d.getY()) / 2, (a.getZ() + d.getZ()) / 2);
+		Vec3 mFace = new Vec3((mAB.getX() + mCD.getX()) / 2, (mAB.getY() + mCD.getY()) / 2,
+				(mAB.getZ() + mCD.getZ()) / 2);
+
+		Vec3 pAB = pointOnSphere(mAB);
+		Vec3 pBC = pointOnSphere(mBC);
+		Vec3 pCD = pointOnSphere(mCD);
+		Vec3 pAD = pointOnSphere(mAD);
+		Vec3 pFace = pointOnSphere(mFace);
+
+		res.add(pFace);
+		res.add(pAD);
 		res.add(a);
-		res.add(p1);
-		
+		res.add(pAB);
 
-		res.add(p5);
-		res.add(p1);
+		res.add(pFace);
+		res.add(pAB);
 		res.add(b);
-		res.add(p2);
+		res.add(pBC);
 
-		res.add(p5);
-		res.add(p2);
+		res.add(pFace);
+		res.add(pBC);
 		res.add(c);
-		res.add(p3);
+		res.add(pCD);
 
-		res.add(p5);
-		res.add(p3);
+		res.add(pFace);
+		res.add(pCD);
 		res.add(d);
-		res.add(p4);
+		res.add(pAD);
 
 		return res;
 	}
